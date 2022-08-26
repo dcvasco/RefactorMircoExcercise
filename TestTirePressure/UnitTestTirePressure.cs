6c643cbb0f7a11fd15ec530baf51cdc59a2bb57f
DanieL Vasco <ddcos7@yahoo.com>
2022-08-26 19:10:13 +0100

    Create Unit Tests Project

---
+++ TestTirePressure/UnitTestTirePressure.cs
@@ -0,0 +1,16 @@
+namespace TestTirePressure
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
